819a070368e361303655ebe1710e8fafdf3005a5
Mebin Mathew <mebinmathew006@gmail.com>
2025-07-16 12:37:27 +0530

    Started with signup view

---
+++ book/users/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+class UserManger(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password,**extra_fields):
+        return self.create_user(email, password, **extra_fields)
+        
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=50)
+    profile_image = models.TextField()
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'email'
+    
+    REQUIRED_FIELDS = ['profile_image','name']
+    
+    objects = UserManger()
+        
+    
---
+++ book/users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+# Create your views here.
+
+
+class SignupView (APIView):
+    def post (self, request):
+        pass
+    
