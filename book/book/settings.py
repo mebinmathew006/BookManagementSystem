e9e6575b6194b18c6ded4138878b147eba18d2f7
Mebin Mathew <mebinmathew006@gmail.com>
2025-07-21 14:42:26 +0530

    moved readme file

--- book/book/settings.py
+++ book/book/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
+print(DEBUG,'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
